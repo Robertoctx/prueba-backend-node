06c5f8633ee291d5d10035719dd78acb5434806d
Roberto Centeno <robertocenteno96@gmail.com>
2021-09-18 10:00:24 -0500

    conexion oracle

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { createConnection } from "typeorm";
+
+const app = express();
+createConnection();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.listen(3000);
+console.log("Server on port: ", 3000);
